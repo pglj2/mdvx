cb3a13cbb1c0244f3915075e5442848815c77a7a
Paulo Lasalvia <pglj2@cin.ufpe.br>
2018-03-13 15:08:28 -0300

    Added project with log4j

---
+++ src/main/java/com/midiavox/mdvx/SwaggerDocumentSetup.java
@@ -0,0 +1,29 @@
+package com.midiavox.mdvx;
+
+import io.swagger.jaxrs.config.BeanConfig;
+
+import javax.servlet.ServletConfig;
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServlet;
+
+public class SwaggerDocumentSetup extends HttpServlet{
+
+	private static final long serialVersionUID = 1L;
+	
+	@Override
+	public void init(ServletConfig config) throws ServletException {
+		super.init(config);
+		
+		BeanConfig beanConfig = new BeanConfig();
+		beanConfig.setVersion("1.0.0");
+		beanConfig.setTitle("MidiaVox, General API's");
+		beanConfig.setDescription("Our core API that can be used for all sorts of stuff. Please Enjoy");
+		beanConfig.setSchemes(new String[]{"http"});
+		beanConfig.setHost("localhost:8080");
+		beanConfig.setBasePath("/mdvx/services");
+		
+		beanConfig.setResourcePackage("com.midiavox.mdvx.services");
+		beanConfig.setScan(true);
+	}
+
+}
---
+++ src/main/java/com/midiavox/mdvx/services/v1/UsersResource.java
@@ -0,0 +1,61 @@
+package com.midiavox.mdvx.services.v1;
+
+import io.swagger.annotations.Api;
+import io.swagger.annotations.ApiOperation;
+import io.swagger.annotations.ApiParam;
+import io.swagger.annotations.ApiResponses;
+import io.swagger.annotations.ApiResponse;
+
+
+
+
+
+
+
+import javax.ws.rs.GET;
+import javax.ws.rs.Path;
+import javax.ws.rs.PathParam;
+import javax.ws.rs.Produces;
+import javax.ws.rs.core.MediaType;
+import javax.ws.rs.core.Response;
+
+import org.apache.log4j.Logger;
+
+import com.midiavox.mdvx.services.v1.User;
+
+@Path("/v1/users")
+@Api(value = "/users", description = "Manage Users")
+
+public class UsersResource {
+	
+	private static final Logger log = Logger.getLogger(UsersResource.class.getName());
+	
+	@GET
+	@Path("/{userId}")
+	@Produces(MediaType.APPLICATION_JSON)
+	@ApiOperation(value = "Find User",
+	notes = "This API retrieves the public information for the user(Private info is returned if this is the auth user)"+
+	"<p><u>Input Parameters</u><ul><li><b>userId</b> is required</li></ul>")
+	@ApiResponses(value = {
+			@ApiResponse(code = 200, message = "Sucess:{ user profile }"),
+			@ApiResponse(code = 400, message = "Failed: {\"error\":\"error_description\", \"status\":\"FAIL\"}")
+	})
+	
+	public Response getUserById(@ApiParam(value = "userId", required = true, defaultValue = "23456", allowableValues = "", allowMultiple = false)
+	@PathParam("userId") String userId){
+		
+		log.info("UsersResource::getUserById started");
+		
+		if (userId == null){
+			return Response.status(Response.Status.BAD_REQUEST)
+					.entity("{\"error\":\"Empty userId\", \"status\":\"FAIL\"}")
+					.build();
+		}
+		User user = new User();
+
+		user.setId("112233");
+		user.setName("Paul Lasalvia");
+		
+		return Response.status(Response.Status.OK).entity(user).build();
+	}
+}
