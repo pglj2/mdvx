8a69653588bbc52aca3d5080824478ec44f87034
Paulo Lasalvia <pglj2@cin.ufpe.br>
2018-03-15 17:08:59 -0300

    Added connection with SQLServerExpress

--- src/main/java/com/midiavox/mdvx/services/v1/User.java
+++ src/main/java/com/midiavox/mdvx/services/v1/User.java
@@ -6,9 +6,8 @@ import javax.xml.bind.annotation.XmlElement;
 import javax.xml.bind.annotation.XmlRootElement;
 
 @XmlRootElement(name = "user")
-
-public class User implements Serializable{
-
+public class User implements Serializable {
+	
 	private static final long serialVersionUID = 1L;
 	
 	@XmlElement(name = "id")
@@ -17,9 +16,11 @@ public class User implements Serializable{
 	@XmlElement(name = "name")
 	private String name;
 	
+	
 	public String getId() {
 		return id;
 	}
+	
 	public void setId(String id) {
 		this.id = id;
 	}
@@ -29,5 +30,6 @@ public class User implements Serializable{
 	public void setName(String name) {
 		this.name = name;
 	}
+	
 	
 }
--- src/main/webapp/index.jsp
+++ src/main/webapp/index.jsp
@@ -1,5 +1,5 @@
 <html>
 <body>
-<h2>Hello World!</h2>
+<h2>MidiaVox!</h2>
 </body>
 </html>
